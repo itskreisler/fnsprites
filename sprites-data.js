8252e74fdd2de25cbdfca6b3e82ea90d78404a07
staticvacant <mewikinn@gmail.com>
2026-06-08 12:32:35 -0500

    Update sprites-data.js

--- sprites-data.js
+++ sprites-data.js
@@ -19,7 +19,31 @@ const baseSprites = [
     { id: "fire_galaxy", name: "Galaxy Fire Sprite", theme: "Galaxy", unreleased: true },
     { id: "fire_holofoil", name: "Holofoil Fire Sprite", theme: "Holofoil", unreleased: true },
 
-    
+    	{ id: "duck_basic", name: "Duck Sprite", theme: "Basic", unreleased: false },
+    { id: "duck_gold", name: "Gold Duck Sprite", theme: "Gold", unreleased: false },
+    { id: "duck_candy", name: "Candy Duck Sprite", theme: "Candy", unreleased: true },
+    { id: "duck_galaxy", name: "Galaxy Duck Sprite", theme: "Galaxy", unreleased: true },
+    { id: "duck_gem", name: "Gem Duck Sprite", theme: "Gem", unreleased: true },
+	
+	{ id: "ghost_basic", name: "Ghost Sprite", theme: "Basic", unreleased: false },
+    { id: "ghost_gold", name: "Gold Ghost Sprite", theme: "Gold", unreleased: false },
+    { id: "ghost_candy", name: "Candy Ghost Sprite", theme: "Candy", unreleased: true },
+    { id: "ghost_galaxy", name: "Galaxy Ghost Sprite", theme: "Galaxy", unreleased: true },
+    { id: "ghost_holofoil", name: "Holofoil Ghost Sprite", theme: "Holofoil", unreleased: true },
+	
+	{ id: "dream_basic", name: "Dream Sprite", theme: "Basic", unreleased: false },
+    { id: "dream_gold", name: "Gold Dream Sprite", theme: "Gold", unreleased: false },
+    { id: "dream_candy", name: "Candy Dream Sprite", theme: "Candy", unreleased: true },
+    { id: "dream_galaxy", name: "Galaxy Dream Sprite", theme: "Galaxy", unreleased: true },
+    { id: "dream_rift", name: "Rift Dream Sprite", theme: "Rift", unreleased: true },
+	
+	{ id: "demon_basic", name: "Demon Sprite", theme: "Basic", unreleased: false },
+    { id: "demon_gold", name: "Gold Demon Sprite", theme: "Gold", unreleased: false },
+    { id: "demon_candy", name: "Candy Demon Sprite", theme: "Candy", unreleased: true },
+    { id: "demon_gem", name: "Gem Demon Sprite", theme: "Gem", unreleased: true },
+
+
+
     
     { id: "theburntpeanut_basic", name: "TheBurntPeanut Sprite", theme: "Basic", unreleased: true },
 ];
